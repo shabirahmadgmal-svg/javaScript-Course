28baca46bed55e557021f8e862c134c9da24c45b
Shabir Ahmad <shabir.ahmadgmal@gmail.com>
2026-01-20 02:52:23 +0500

    first

---
+++ day1/variables.js
@@ -0,0 +1,25 @@
+/*  Variables : 
+          variables are container for storing data and values.
+          there are 3 types of variables 
+          1. var
+          2. let
+          3. const
+*/
+const id = 120984 ;
+// variable whose value cannot be changed
+let fullName = "Shabir Ahmad" ;
+// variable whose value can be changed and not redeclared
+var age = 25 ;
+// variable whose value can be changed and can be redeclared
+address = " Islamabad " ;
+// variable without var, let, const keyword is global variable
+
+// id = 34534534; Error Not allowed
+// name = "Shabir" ; allowed`
+//var age = 35 ;  redeclared age variable AND CHANGED value
+/*
+ use of var variable is not recommended
+ avoid using var variable
+*/
+console.table([id, fullName, age, address]) ;
+
